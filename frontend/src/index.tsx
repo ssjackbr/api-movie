939a93cb73268217811de7b984e9232cff3bddfa
Jackson Rodrigues <jjr0910@gmail.com>
2022-01-11 22:41:45 -0300

    Test add pull reques template

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-//imports
+
 
 ReactDOM.render(
     <React.StrictMode>
